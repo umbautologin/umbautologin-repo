86d889f170c5ef5f822bf4d169b3b40d62d6fe0c
umbautologin <umbautologin@gmail.com>
2012-10-19 00:40:17 -0400

    more modified files

--- umbautologin-app/gen/the/umbautologin/R.java
+++ umbautologin-app/gen/the/umbautologin/R.java
@@ -12,22 +12,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int iconold=0x7f020001;
     }
     public static final class id {
-        public static final int active=0x7f060001;
+        public static final int active=0x7f060002;
         public static final int clear_hist_menu_item=0x7f06000b;
-        public static final int histTable=0x7f060003;
-        public static final int prefs_checkbox_already_logged=0x7f060006;
-        public static final int prefs_checkbox_error=0x7f060005;
-        public static final int prefs_checkbox_success=0x7f060004;
-        public static final int prefs_passwd=0x7f060008;
+        public static final int histTable=0x7f060004;
+        public static final int prefs_checkbox_already_logged=0x7f060007;
+        public static final int prefs_checkbox_error=0x7f060006;
+        public static final int prefs_checkbox_success=0x7f060005;
+        public static final int prefs_passwd=0x7f060009;
         public static final int prefs_save=0x7f06000a;
-        public static final int prefs_url=0x7f060009;
-        public static final int prefs_username=0x7f060007;
+        public static final int prefs_username=0x7f060008;
         public static final int settings=0x7f06000c;
         public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060002;
+        public static final int textView2=0x7f060003;
+        public static final int textView3=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -38,27 +37,29 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int clear_history=0x7f040005;
-        public static final int conf_save=0x7f04000c;
-        public static final int defaultpasswd=0x7f040016;
-        public static final int defaulturl=0x7f040012;
-        public static final int defaultusername=0x7f040014;
+        public static final int clear_history=0x7f040006;
+        public static final int conf_save=0x7f04000d;
+        public static final int defaultpasswd=0x7f040018;
+        public static final int defaulturl=0x7f040014;
+        public static final int defaultusername=0x7f040016;
+        public static final int desc=0x7f040003;
         public static final int hello=0x7f040000;
-        public static final int history=0x7f040003;
-        public static final int nohist=0x7f040004;
-        public static final int notify_message_already_logged=0x7f040010;
-        public static final int notify_message_error=0x7f04000f;
-        public static final int notify_message_success=0x7f04000e;
-        public static final int notify_prefs_already_logged=0x7f04000a;
-        public static final int notify_prefs_error=0x7f040009;
-        public static final int notify_prefs_success=0x7f040008;
-        public static final int notify_prefs_title=0x7f040007;
-        public static final int notify_title=0x7f04000d;
-        public static final int passwdlabel=0x7f040015;
-        public static final int save=0x7f04000b;
-        public static final int settings=0x7f040006;
+        public static final int history=0x7f040004;
+        public static final int nohist=0x7f040005;
+        public static final int notify_message_already_logged=0x7f040011;
+        public static final int notify_message_error=0x7f040010;
+        public static final int notify_message_success=0x7f04000f;
+        public static final int notify_prefs_already_logged=0x7f04000b;
+        public static final int notify_prefs_error=0x7f04000a;
+        public static final int notify_prefs_success=0x7f040009;
+        public static final int notify_prefs_title=0x7f040008;
+        public static final int notify_title=0x7f04000e;
+        public static final int notify_umbguest_error=0x7f040012;
+        public static final int passwdlabel=0x7f040017;
+        public static final int save=0x7f04000c;
+        public static final int settings=0x7f040007;
         public static final int title=0x7f040002;
-        public static final int urllabel=0x7f040011;
-        public static final int usernamelabel=0x7f040013;
+        public static final int urllabel=0x7f040013;
+        public static final int usernamelabel=0x7f040015;
     }
 }
